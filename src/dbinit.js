a5215b3a653fd8fa9a07f84c83c9a94d10502cfa
4erem6a <Sviridoff_E@mail.ru>
2019-03-15 00:29:48 +0300

    Update model loading

--- src/dbinit.js
+++ src/dbinit.js
@@ -4,11 +4,7 @@ import requireAll from "require-all";
 export default async function dbinit() {
     const sequelize = new Sequelize({ dialect: "sqlite" });
 
-    const models = loadModels(sequelize, `${__dirname}/models`);
-
-    Object.values(models)
-        .filter(model => typeof model.associate === "function")
-        .forEach(model => model.associate(models));
+    loadModels(sequelize, `${__dirname}/models`);
     
     await sequelize.sync();
 
@@ -16,11 +12,9 @@ export default async function dbinit() {
 }
 
 function loadModels(sequelize, dirname) {
-    const models = {};
-    const directory = requireAll({ dirname });
-    for (const filename in directory) {
-        const model = directory[filename].default || directory[filename];
-        Object.assign(models, { [model.name]: model.init(sequelize) });
-    }
-    return models;
+    Object.values(requireAll({ dirname }))
+        .map(model => model.default || model)
+        .map(model => model.init(sequelize))
+        .filter(model => typeof model.associate === "function")
+        .forEach((model, i, models) => model.associate(models));
 }
