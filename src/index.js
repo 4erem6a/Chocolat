587d4532a0e2a567ba76885ca0bbe31cfa6c67b7
4erem6a <Sviridoff_E@mail.ru>
2019-03-15 17:04:00 +0300

    Implemented i18next

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import { Client } from "discord.js";
+import {
+    Client
+} from "discord.js";
 
 import loadCommands from "./processing/commands/loadCommands";
-
 import loadEvents from "./processing/events/loadEvents";
 
 import dbinit from "./dbinit";
 
+import i18next from "i18next";
+import Backend from "i18next-node-fs-backend";
+
 if (process.env.NODE_ENV == "development") {
     require("dotenv").config();
 }
+
+i18next
+    .use(Backend)
+    .init({
+        fallbackLng: "en",
+        ns: ["system", "commands", "messages"],
+        initImmediate: false,
+        backend: {
+            loadPath: `${__dirname}/../locales/{{lng}}/{{ns}}.yml`
+        }
+    }).then(() => console.log("Translations loaded"));
 
 const client = new Client();
 
@@ -18,9 +33,8 @@ loadEvents(client, `${__dirname}/events`);
 
 client.once("ready", async () => {
     client.sequelize = await dbinit();
+    client.synced = true;
     client.emit("sync");
 });
-
-client.once("sync", () => client.synced = true);
 
 client.login(process.env.TOKEN).catch(console.error);
