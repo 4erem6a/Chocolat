460967a5ae310cff083b3ec924ef01cb588306be
4erem6a <Sviridoff_E@mail.ru>
2019-03-15 21:44:36 +0300

    Update ping command

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -1,15 +1,14 @@
 import Command from "../../processing/commands/Command";
 import { name, description } from "../../processing/commands/decorators";
-import { ls } from "../../utils/LocalizedString";
-import i18next from "i18next";
+import { ls, ll } from "../../utils/LocalizedString";
 
 @name("ping")
 @description(ls`commands:ping.description`)
 export default class PingCommand extends Command {
     async run(message) {
-        const pingMessage = await message.channel.send(i18next.t("commands:ping.messages.pinging"));
+        const pingMessage = await message.channel.send(ll`commands:ping.messages.pinging`());
         const latency = pingMessage.createdTimestamp - message.createdTimestamp;
         const apiLatency = Math.round(this.client.ping);
-        pingMessage.edit(i18next.t("commands:ping.messages.result", { latency, apiLatency }));
+        pingMessage.edit(ll`commands:ping.messages.result`({ latency, apiLatency }));
     }
 }
