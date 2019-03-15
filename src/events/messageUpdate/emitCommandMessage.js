5c50e37b1f209b0df6adac55bfa08bfa8dbeaee4
4erem6a <Sviridoff_E@mail.ru>
2019-03-15 16:58:10 +0300

    Remove redundant checks

--- src/events/messageUpdate/emitCommandMessage.js
+++ src/events/messageUpdate/emitCommandMessage.js
@@ -1,7 +1,4 @@
 export default (client, oldMessage, newMessage) => {
-    if (newMessage.author.bot || newMessage.system) {
-        return;
-    }
     if (oldMessage.content == newMessage.content || !newMessage.content) {
         return;
     }
