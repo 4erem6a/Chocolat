4a3cf88e36775f2812e26dddc8c80a0f218bfd41
4erem6a <Sviridoff_E@mail.ru>
2019-03-16 22:52:20 +0300

    Add voicexp::time command

---
+++ src/commands/voicexp/time.js
@@ -0,0 +1,42 @@
+import Command from "../../processing/commands/Command";
+import { name, description, group, format, guildOnly } from "../../processing/commands/decorators";
+import { ls, ll } from "../../utils/LocalizedString";
+import GuildMember from "../../models/GuildMember";
+import ms from "ms";
+import { parseMember } from "./../../utils/parseMention";
+
+@name("time")
+@group("voicexp")
+@description(ls`commands:voicexp.time.description`)
+@format(ls`commands:voicexp.time.format`)
+@guildOnly
+export default class PingCommand extends Command {
+    async run(message, [member]) {
+        if (!member) {
+            const [guildMember] = await GuildMember.findOrBuild({
+                where: { userId: message.author.id, guildId: this.guild.id },
+                defaults: { userId: message.author.id, guildId: this.guild.id }
+            });
+
+            return ll`commands:voicexp.time.messages.get`({
+                time: ms(guildMember.voiceTime, { long: true })
+            });
+        }
+
+        member = parseMember(this.guild, member);
+
+        if (!member) {
+            return ll`commands:voicexp.time.messages.memberNotFound`();
+        }
+
+        const [guildMember] = await GuildMember.findOrBuild({
+            where: { userId: member.user.id, guildId: this.guild.id },
+            defaults: { userId: member.user.id, guildId: this.guild.id }
+        });
+
+        return ll`commands:voicexp.time.messages.member`({
+            member,
+            time: ms(guildMember.voiceTime, { long: true })
+        });
+    }
+}
